a71cfc53f41ed0ddebd174a0b25f2fd7203342c8
Emmanuel Okeke <emmanix2002@gmail.com>
2018-03-13 16:08:10 +0100

    added the metrics service

---
+++ src/Services/Metrics.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Hostville\Dorcas\Services;
+
+
+class Metrics extends AbstractService
+{
+    /**
+     * @inheritdoc
+     */
+    public function requiresAuthorization(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Returns the name of the service.
+     *
+     * @return string
+     */
+    function getName(): string
+    {
+        return 'Metrics';
+    }
+}
