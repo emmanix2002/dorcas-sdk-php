ce9c8128f6b65930dcfc74eb930e9cda336fdeec
Emmanuel Okeke <emmanix2002@gmail.com>
2018-04-02 14:17:23 +0100

    added support for plans in the registration flow

--- src/helpers.php
+++ src/helpers.php
@@ -139,7 +139,7 @@ function login_via_password(Hostville\Dorcas\Sdk $sdk, string $username, string 
 function create_account(\Hostville\Dorcas\Sdk $sdk, array $config): \Hostville\Dorcas\DorcasResponse
 {
     $service = $sdk->createRegistrationService();
-    $keys = ['email', 'firstname', 'lastname', 'password', 'phone', 'company'];
+    $keys = ['email', 'firstname', 'lastname', 'password', 'phone', 'company', 'plan'];
     # the config keys we need
     foreach ($keys as $key) {
         if (!array_key_exists($key, $config)) {
